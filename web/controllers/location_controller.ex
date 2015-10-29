ea40338d37d7489b64235bc62d0c98dfde18cbe0
Joshua Clayton <jclayton@thoughtbot.com>
2015-10-28 21:16:46 -0400

    Allow limiting results

--- web/controllers/location_controller.ex
+++ web/controllers/location_controller.ex
@@ -2,8 +2,14 @@ defmodule Geocoding.LocationController do
   use Geocoding.Web, :controller
   alias Geocoding.Location
 
+  def index(conn, %{"limit" => limit}) do
+    locations = Ecto.Query.from(l in Location, select: l, limit: ^limit)
+                |> Repo.all
+
+    render conn, "index.html", locations: locations
+  end
+
   def index(conn, _params) do
-    # locations = Ecto.Query.from(l in Location, select: l, limit: 50) |> Repo.all
     locations = Repo.all(Location)
 
     render conn, "index.html", locations: locations
