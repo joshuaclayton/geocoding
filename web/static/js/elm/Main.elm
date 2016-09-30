fbca5965a523adb245b5d8108cb41d9c81d32f94
Joshua Clayton <joshua.clayton@gmail.com>
2016-09-30 13:15:38 -0400

    Render locations via bootstrapped data with Elm

--- web/static/js/elm/Main.elm
+++ web/static/js/elm/Main.elm
@@ -4,11 +4,55 @@ import Html exposing (..)
 import Html.App
 
 
-main : Program Never
+type alias Store =
+    { name : String
+    , latLong : LatLong
+    , phoneNumber : String
+    }
+
+
+type alias LatLong =
+    ( Float, Float )
+
+
+type alias Model =
+    List Store
+
+
+type alias Flags =
+    { locations : List { name : String, lat : Float, long : Float, phone : String }
+    }
+
+
+flagsToModel : Flags -> Model
+flagsToModel flags =
+    let
+        flagToStore f =
+            Store f.name ( f.lat, f.long ) f.phone
+    in
+        List.map flagToStore flags.locations
+
+
+init : Flags -> ( Model, Cmd a )
+init flags =
+    ( flagsToModel flags, Cmd.none )
+
+
+view : Model -> Html a
+view model =
+    ul [] <| List.map storeEntry model
+
+
+storeEntry : Store -> Html a
+storeEntry store =
+    li [] [ text store.name ]
+
+
+main : Program Flags
 main =
-    Html.App.program
-        { init = ( "", Cmd.none )
+    Html.App.programWithFlags
+        { init = init
         , update = (\_ m -> ( m, Cmd.none ))
-        , view = (\_ -> div [] [ text "well, hello world" ])
+        , view = view
         , subscriptions = (\_ -> Sub.none)
         }
